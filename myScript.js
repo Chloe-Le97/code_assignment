3de1adc7c6364c7847adc72f05b901ac31d8634b
Chloe-Le97 <phuonganhkitty365@gmail.com>
2021-02-12 00:35:26 +0200

    update latest news according to the RSS

--- myScript.js
+++ myScript.js
@@ -4,7 +4,6 @@ function onClickMenu() {
     document.getElementById("hamburger").classList.toggle("change");
     document.getElementById("nav").classList.toggle("changenav");
     document.getElementById("hamburger-bg").classList.toggle("change-bg");
-    document.getElementById("main").classList.toggle("change-body");
     document.querySelector("body").classList.toggle("change-bod");
 }
 
@@ -12,13 +11,25 @@ function CloseMenu() {
     document.getElementById("hamburger").classList.remove("change");
     document.getElementById("nav").classList.remove("changenav");
     document.getElementById("hamburger-bg").classList.remove("change-bg");
-    document.getElementById("main").classList.remove("change-body");
     document.querySelector("body").classList.remove("change-bod");
 }
 
+// logo position
+window.addEventListener('scroll',(event)=>{
+    event.preventDefault();
+    if (window.scrollY>=10){
+        document.querySelector("#logo").style.top="0";
+        document.querySelector("#hamburger").style.top="30px";
+    }
+    else{
+        document.querySelector("#logo").style.top="60px";
+        document.querySelector("#hamburger").style.top="90px"
+    }
+});
+
 
 // animated number 
-window.addEventListener('scroll',function(event){
+window.addEventListener('scroll',(event)=>{
     event.preventDefault();
     if (window.scrollY>=400){
         const counters = document.querySelectorAll(".number_text_number")
@@ -62,22 +73,32 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://www.lian
 .then(data => data.contents)
 .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
 .then(result=>{
-    console.log(result)
     const items = result.querySelectorAll("item");
     const news_details= document.querySelectorAll(".news_details_title");
     const date_publish = document.querySelectorAll(".news_date");
     const news_link = document.querySelectorAll(".news_link");
-    console.log(news_details)
     for (let i = 0; i < 4; i++) {
         let item = items[i];
         let news_detail = news_details[i];
         let date = date_publish[i];
         let link = item.querySelector("link").innerHTML;
         news_link[i].setAttribute('href',link);
-        console.log(news_link[i])
         const parseDate = item.querySelector("pubDate").innerHTML.split(' ');
         news_detail.innerHTML = item.querySelector("title").innerHTML;
         date.innerHTML = `${parseDate[1]} ${parseDate[2]} ${parseDate[3]}`;
       }
 });
 
+const submitForm = () =>{
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    const emailInput = document.querySelector(".email_input");
+    if(emailInput.value==''){alert('Please fill your email to subscribe to our latest news')}
+    else if(!emailInput.value.match(mailformat)){alert('Please provide a valid email address')}
+    else{
+        const subcribe_noti = document.querySelector(".subcribe_noti");
+        subcribe_noti.style.display="block";
+        setTimeout(()=>{subcribe_noti.style.display="none"},3000);
+    }
+
+    emailInput.value='';
+}
